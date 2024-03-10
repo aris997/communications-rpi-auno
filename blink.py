0deb243e13b70d7b6d8eb17f70b4a0434df7f1e7
Riccardo Riva <riccardo.riva.997@gmail.com>
2024-03-10 21:05:34 +0100

    rivaR: updates blinker py reciver

--- blink.py
+++ blink.py
@@ -7,8 +7,12 @@ GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(PIN_BLK, GPIO.OUT, initial=GPIO.LOW)
 
+blinks = 0
+print('Number of blinks:')
 while True:
 	GPIO.output(PIN_BLK, GPIO.HIGH)
 	sleep(1)
 	GPIO.output(PIN_BLK, GPIO.LOW)
 	sleep(1)
+	print(blinks, end='\r')
+	blinks += 1
